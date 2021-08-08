843be375a5ec93e5804599d0267617a8addcfa9a
Sean Baier <sean.t.baier@gmail.com>
2021-08-08 11:01:58 -0500

    moving modules

---
+++ cloudwatch/variables.tf
@@ -0,0 +1,10 @@
+variable "environment" {
+  description = "Environment resource is used in"
+  type        = string
+
+}
+
+variable "app_name" {
+  description = "Name of the application"
+  type        = string
+}
