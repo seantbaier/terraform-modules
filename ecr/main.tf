78968622ffc02b117602d4c7d9152815d37fcbee
Sean Baier <sean.t.baier@gmail.com>
2021-08-08 12:27:15 -0500

    fixing repo address

--- ecr/main.tf
+++ ecr/main.tf
@@ -1,7 +1,12 @@
+data "aws_region" "current" {}
+
+data "aws_caller_identity" "this" {}
+
 data "aws_ecr_authorization_token" "token" {}
 
 locals {
-  image_tag = "latest"
+  image_tag          = "latest"
+  repository_address = "${data.aws_caller_identity.this.account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com"
 }
 
 resource "aws_ecr_repository" "this" {
@@ -18,14 +23,14 @@ resource "aws_ecr_repository" "this" {
 
 provider "docker" {
   registry_auth {
-    address  = aws_ecr_repository.this.repository_url
+    address  = local.repository_address
     username = data.aws_ecr_authorization_token.token.user_name
     password = data.aws_ecr_authorization_token.token.password
   }
 }
 
 resource "docker_registry_image" "this" {
-  name = "${aws_ecr_repository.this.repository_url}/${var.name}:${local.image_tag}"
+  name = "${local.repository_address}/${var.name}:${local.image_tag}"
 
   build {
     context    = "../context"
