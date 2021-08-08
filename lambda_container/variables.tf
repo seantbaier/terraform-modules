4d7801ba4bc006cabcf537855df5eba20baca080
Sean Baier <sean.t.baier@gmail.com>
2021-08-08 14:38:54 -0500

    lambda-container

--- lambda_container/variables.tf
+++ lambda_container/variables.tf
@@ -13,8 +13,3 @@ variable "function_name" {
   description = "Name of function"
   type        = string
 }
-
-variable "image_uri" {
-  description = "Image uri for lambda docker container"
-  type = string
-}
