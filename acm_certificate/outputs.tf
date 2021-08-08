6f04834eca95760867f738220789d4f7216e4ebe
Sean Baier <sean.t.baier@gmail.com>
2021-08-08 13:25:02 -0500

    adding dns

--- acm_certificate/outputs.tf
+++ acm_certificate/outputs.tf
@@ -2,11 +2,11 @@
 
 output "arn" {
   description = "ARN for the SSL certificate"
-  value       = aws_acm_certificate.this[0].arn
+  value       = aws_acm_certificate.this.arn
 }
 
 
 output "domain_validation_options" {
   description = "Set of domain validation objects which can be used to complete certificate validation. Can have more than one element, e.g. if SANs are defined. Only set if DNS-validation was used."
-  value       = aws_acm_certificate.this[0].domain_validation_options
+  value       = aws_acm_certificate.this.domain_validation_options
 }
