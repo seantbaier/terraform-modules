94be91141375ed451a6dcd3e39c86bcb1702da56
Sean Baier <sean.t.baier@gmail.com>
2021-08-14 13:10:53 -0500

    fixed reference this

--- aws_route53/validation_record/outputs.tf
+++ aws_route53/validation_record/outputs.tf
@@ -1,4 +1,4 @@
 output "validation_record_fqdns" {
   description = "AWS Route53 Validation Record fqdns"
-  value       = aws_route53_record[0].validation.*.fqdn
+  value       = aws_route53_record.this.[0].validation.*.fqdn
 }
