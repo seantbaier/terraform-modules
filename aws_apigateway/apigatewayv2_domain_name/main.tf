6f4b3bb7773d9cdcef4e934f10fe5e851b1ed881
Sean Baier <sean.t.baier@gmail.com>
2021-08-14 15:14:40 -0500

    added outputs

--- aws_apigateway/apigatewayv2_domain_name/main.tf
+++ aws_apigateway/apigatewayv2_domain_name/main.tf
@@ -2,14 +2,14 @@ resource "aws_apigatewayv2_domain_name" "this" {
   domain_name = var.domain_name
 
   domain_name_configuration {
-    certificate_arn = var.certificate_arn
+    certificate_arn = var.aws_acm_certificate_arn
     endpoint_type   = "REGIONAL"
     security_policy = "TLS_1_2"
   }
 }
 
 resource "aws_apigatewayv2_api_mapping" "this" {
-  api_id      = aws_apigatewayv2_api.this.id
+  api_id      = var.aws_apigatewayv2_api_id
   domain_name = aws_apigatewayv2_domain_name.this.id
-  stage       = aws_apigatewayv2_stage.this.id
+  stage       = var.aws_apigatewayv2_stage_id
 }
--- aws_apigateway/apigatewayv2_domain_name/variables.tf
+++ aws_apigateway/apigatewayv2_domain_name/variables.tf
@@ -1,30 +1,15 @@
-variable "environment" {
-  description = "Environment the resource is provisioned in."
-  type        = string
-}
-
-variable "name" {
-  description = "Name for the AWS resource."
-  type        = string
-}
-
 variable "domain_name" {
   description = "(Required) The domain name. Must be between 1 and 512 characters in length."
   type        = string
 }
 
-variable "certificate_arn" {
+variable "aws_acm_certificate_arn" {
   description = "(Required) The ARN of an AWS-managed certificate that will be used by the endpoint for the domain name. AWS Certificate Manager is the only supported source. Use the aws_acm_certificate resource to configure an ACM certificate."
   type        = string
 }
 
 variable "api_id" {
   description = "API Gateway Id for aws_apigatewayv2_api_mapping"
-  type = string
-}
-
-variable "aws_apigatewayv2_api_mapping_domain_name_id" {
-  description = "API Gateway domain name id for the aws_apigatewayv2_api_mapping"
   type = string
 }
 
