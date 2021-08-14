aa18740d8a8b1016c8d3e67d4c3539d0ce39fd29
Sean Baier <sean.t.baier@gmail.com>
2021-08-14 09:36:56 -0500

    wrong var name

--- aws_route53/hosted_zone/variables.tf
+++ aws_route53/hosted_zone/variables.tf
@@ -7,9 +7,3 @@ variable "environment" {
   description = "Name of the environment the resource is provisioned in"
   type        = string
 }
-
-
-variable "domain_name" {
-  description = "(Required) The domain name. Must be between 1 and 512 characters in length."
-  type        = string
-}
