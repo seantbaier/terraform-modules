0cb1144829673b31903512ef08d6d4a9c54154f2
Sean Baier <sean.t.baier@gmail.com>
2021-08-14 13:03:55 -0500

    added validation record output

---
+++ aws_acm/certificate_validation/variables.tf
@@ -0,0 +1,9 @@
+variable "certificate_arn" {
+  description = "AWS ACM Certifcate ARN"
+  type = string
+}
+
+variable "validation_record_fqdns" {
+  description = "AWS Route53 Validation Record fqdns"
+  type = list(any)
+}
