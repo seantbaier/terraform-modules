db00b05d8d5ddb8e0ecb32058880f1a2624324df
Sean Baier <sean.t.baier@gmail.com>
2021-08-14 09:47:29 -0500

    wrong output

--- aws_ecr/repository/outputs.tf
+++ aws_ecr/repository/outputs.tf
@@ -1,4 +1,4 @@
-variable "repository_url" {
+output "repository_url" {
   description = "repository_url - The URL of the repository (in the form aws_account_id.dkr.ecr.region.amazonaws.com/repositoryName)."
   value       = aws_ecr_repository.this.repository_url
 }
