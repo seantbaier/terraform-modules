98a8137cbe459f7ad149b50457c4ebe3ee268b7d
Sean Baier <sean.t.baier@gmail.com>
2021-08-15 03:44:19 -0500

    moving validation into the cert module

--- aws_acm/certificate/variables.tf
+++ aws_acm/certificate/variables.tf
@@ -15,17 +15,61 @@ variable "environment" {
   type        = string
 }
 
+variable "validate_certificate" {
+  description = "Whether to validate certificate by creating Route53 record"
+  type        = bool
+  default     = true
+}
+
+variable "validation_allow_overwrite_records" {
+  description = "Whether to allow overwrite of Route53 records"
+  type        = bool
+  default     = true
+}
+
+variable "wait_for_validation" {
+  description = "Whether to wait for the validation to complete"
+  type        = bool
+  default     = true
+}
+
+variable "certificate_transparency_logging_preference" {
+  description = "Specifies whether certificate details should be added to a certificate transparency log"
+  type        = bool
+  default     = true
+}
+
 variable "domain_name" {
-  description = "Domain name for the certifcate."
+  description = "A domain name for which the certificate should be issued"
   type        = string
+  default     = ""
 }
 
 variable "subject_alternative_names" {
-  description = "List of alternative sub domains to be validated against."
-  type        = list(any)
+  description = "A list of domains that should be SANs in the issued certificate"
+  type        = list(string)
+  default     = []
 }
 
 variable "validation_method" {
-  description = "The type of method used to validate the certificate. DNS or EMAIL"
+  description = "Which method to use for validation. DNS or EMAIL are valid, NONE can be used for certificates that were imported into ACM and then into Terraform."
   type        = string
+  default     = "DNS"
+}
+
+variable "zone_id" {
+  description = "The ID of the hosted zone to contain this record."
+  type        = string
+}
+
+variable "tags" {
+  description = "A mapping of tags to assign to the resource"
+  type        = map(string)
+  default     = {}
+}
+
+variable "dns_ttl" {
+  description = "The TTL of DNS recursive resolvers to cache information about this record."
+  type        = number
+  default     = 60
 }
