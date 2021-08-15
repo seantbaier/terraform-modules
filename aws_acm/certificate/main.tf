8489700811fedd2eb650132a97f61627a700964b
Sean Baier <sean.t.baier@gmail.com>
2021-08-15 05:24:36 -0500

    switching back to adding validation in cert module

--- aws_acm/certificate/main.tf
+++ aws_acm/certificate/main.tf
@@ -1,36 +1,3 @@
-resource "aws_acm_certificate" "this" {
-  count = var.create ? 1 : 0
-
-  domain_name               = var.domain_name
-  subject_alternative_names = var.subject_alternative_names
-  validation_method         = var.validation_method
-
-  tags = {
-    App         = var.app_name
-    Terraform   = true
-    Environment = var.environment
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-# locals {
-#   # Get distinct list of domains and SANs
-#   distinct_domain_names = distinct(
-#     [for s in concat([var.domain_name], var.subject_alternative_names) : replace(s, "*.", "")]
-#   )
-
-#   # Get the list of distinct domain_validation_options, with wildcard
-#   # domain names replaced by the domain name
-#   validation_domains = var.create ? distinct(
-#     [for k, v in aws_acm_certificate.this[0].domain_validation_options : merge(
-#       tomap(v), { domain_name = replace(v.domain_name, "*.", "") }
-#     )]
-#   ) : []
-# }
-
 # resource "aws_acm_certificate" "this" {
 #   count = var.create ? 1 : 0
 
@@ -38,38 +5,71 @@ resource "aws_acm_certificate" "this" {
 #   subject_alternative_names = var.subject_alternative_names
 #   validation_method         = var.validation_method
 
-#   options {
-#     certificate_transparency_logging_preference = var.certificate_transparency_logging_preference ? "ENABLED" : "DISABLED"
+#   tags = {
+#     App         = var.app_name
+#     Terraform   = true
+#     Environment = var.environment
 #   }
-
-#   tags = var.tags
 
 #   lifecycle {
 #     create_before_destroy = true
 #   }
 # }
 
-# resource "aws_route53_record" "validation" {
-#   count = var.create && var.validation_method == "DNS" && var.validate_certificate ? length(local.distinct_domain_names) : 0
+locals {
+  # Get distinct list of domains and SANs
+  distinct_domain_names = distinct(
+    [for s in concat([var.domain_name], var.subject_alternative_names) : replace(s, "*.", "")]
+  )
 
-#   zone_id = var.zone_id
-#   name    = element(local.validation_domains, count.index)["resource_record_name"]
-#   type    = element(local.validation_domains, count.index)["resource_record_type"]
-#   ttl     = var.dns_ttl
+  # Get the list of distinct domain_validation_options, with wildcard
+  # domain names replaced by the domain name
+  validation_domains = var.create ? distinct(
+    [for k, v in aws_acm_certificate.this[0].domain_validation_options : merge(
+      tomap(v), { domain_name = replace(v.domain_name, "*.", "") }
+    )]
+  ) : []
+}
 
-#   records = [
-#     element(local.validation_domains, count.index)["resource_record_value"]
-#   ]
+resource "aws_acm_certificate" "this" {
+  count = var.create ? 1 : 0
 
-#   allow_overwrite = var.validation_allow_overwrite_records
+  domain_name               = var.domain_name
+  subject_alternative_names = var.subject_alternative_names
+  validation_method         = var.validation_method
 
-#   depends_on = [aws_acm_certificate.this]
-# }
+  options {
+    certificate_transparency_logging_preference = var.certificate_transparency_logging_preference ? "ENABLED" : "DISABLED"
+  }
 
-# resource "aws_acm_certificate_validation" "this" {
-#   count = var.create && var.validation_method == "DNS" && var.validate_certificate && var.wait_for_validation ? 1 : 0
+  tags = var.tags
 
-#   certificate_arn = aws_acm_certificate.this[0].arn
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-#   validation_record_fqdns = aws_route53_record.validation.*.fqdn
-# }
+resource "aws_route53_record" "validation" {
+  count = var.create && var.validation_method == "DNS" && var.validate_certificate ? length(local.distinct_domain_names) : 0
+
+  zone_id = var.zone_id
+  name    = element(local.validation_domains, count.index)["resource_record_name"]
+  type    = element(local.validation_domains, count.index)["resource_record_type"]
+  ttl     = var.dns_ttl
+
+  records = [
+    element(local.validation_domains, count.index)["resource_record_value"]
+  ]
+
+  allow_overwrite = var.validation_allow_overwrite_records
+
+  depends_on = [aws_acm_certificate.this]
+}
+
+resource "aws_acm_certificate_validation" "this" {
+  count = var.create && var.validation_method == "DNS" && var.validate_certificate && var.wait_for_validation ? 1 : 0
+
+  certificate_arn = aws_acm_certificate.this[0].arn
+
+  validation_record_fqdns = aws_route53_record.validation.*.fqdn
+}
--- aws_route53/hosted_zone/outputs.tf
+++ aws_route53/hosted_zone/outputs.tf
@@ -2,3 +2,8 @@ output "zone_id" {
   description = "Id for the created route53 zone."
   value       = aws_route53_zone.this.zone_id
 }
+
+output "nameservers" {
+  description = "name_servers - A list of name servers in associated (or default) delegation set. Find more about delegation sets in AWS docs."
+  value = aws_route53_zone.this.nameservers
+}
