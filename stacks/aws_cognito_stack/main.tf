b23791626c352bb7ffa13c8f627b50ae84835fd5
Sean Baier <sean.t.baier@gmail.com>
2023-02-05 07:41:10 -0600

    Initial setup

--- stacks/aws_cognito_stack/main.tf
+++ stacks/aws_cognito_stack/main.tf
@@ -1,6 +1,20 @@
-module "aws_cognito_ecr_repository" {
+locals {
+  post_confirmation_trigger_lambda_name = "${var.name}-post-confirmation-trigger"
+}
+
+
+module "aws_cognito_post_confirmation_trigger_repository" {
   source = "../../modules/aws/aws_ecr/aws_ecr_repository"
 
-  name        = "${var.name}-ecr-repository"
+  name        = local.post_confirmation_trigger_lambda_name
   environment = var.environment
+}
+
+
+module "aws_cognito_post_confirmation_trigger_image" {
+  source = "../../modules/docker/docker_registry_image"
+
+  name               = local.post_confirmation_trigger_lambda_name
+  environment        = var.environment
+  repository_address = module.aws_cognito_post_confirmation_trigger_repository.repository_url
 }
