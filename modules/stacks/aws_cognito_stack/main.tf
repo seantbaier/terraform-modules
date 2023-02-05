1df4d4989a68b3674ed4033163a153ae981cf2b8
Sean Baier <sean.t.baier@gmail.com>
2023-02-05 07:32:07 -0600

    Initial setup

--- modules/stacks/aws_cognito_stack/main.tf
+++
@@ -1,6 +0,0 @@
-module "aws_cognito_ecr_repository" {
-  source = "../../aws/aws_ecr/aws_ecr_repository"
-
-  name = "${var.name}-ecr-repository"
-  env  = var.environment
-}
---
+++ stacks/aws_cognito_stack/main.tf
@@ -0,0 +1,6 @@
+module "aws_cognito_ecr_repository" {
+  source = "../../modules/aws/aws_ecr/aws_ecr_repository"
+
+  name        = "${var.name}-ecr-repository"
+  environment = var.environment
+}
