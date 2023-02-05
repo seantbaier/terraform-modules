4aa7fd8195649d983befc51e3b6e07cee1bd6e80
Sean Baier <sean.t.baier@gmail.com>
2023-02-05 07:26:27 -0600

    Initial setup

---
+++ stacks/aws_cognito_stack/variables.tf
@@ -0,0 +1,10 @@
+variable "environment" {
+  description = "Environment resource is used in"
+  type        = string
+
+}
+
+variable "name" {
+  description = "Name for stack resources"
+  type        = string
+}
